1407ae1a04aa5c9121009912aacea7f6581aa0b6
NadavAharoni <nadav.aharoni@gmail.com>
2025-12-09 17:56:33 +0200

    Test c# internal functions

---
+++ Module_003/OperationTable/InternalFunction.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+class InternalFunction
+{
+    public delegate double UnaryOperator(double d);
+    public static UnaryOperator RetOp(double c)
+    {
+        UnaryOperator f = (double x) => { return x + c; };
+        return f;
+    }
+
+    public static UnaryOperator RetOp2(double c)
+    {
+        double LocalFunction(double x)
+        {
+            return x + c;
+        }
+        return LocalFunction;
+    }
+
+
+    public static void Test()
+    {
+        UnaryOperator op = RetOp(3.0);
+        double result = op(4.0);
+        Console.WriteLine($"Result: {result}");
+
+        UnaryOperator op2 = RetOp(10.0);
+        double result2 = op2(5.0);
+        Console.WriteLine($"Result: {result2}");
+    }
+}
