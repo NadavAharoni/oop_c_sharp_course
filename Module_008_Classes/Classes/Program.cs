e8d1fb220ab549e02111fe2f025dbcbf65d59a48
NadavAharoni <nadav.aharoni@gmail.com>
2026-01-12 22:38:13 +0200

    Moved BankAccount class to its own file

--- Module_008_Classes/Classes/Program.cs
+++ Module_008_Classes/Classes/Program.cs
@@ -1,32 +1,7 @@
-﻿namespace Classes
+﻿using System;
+
+namespace Classes
 {
-    class BankAccount
-    {
-        private decimal balance;
-        public BankAccount(decimal initialBalance)
-        {
-            balance = initialBalance;
-        }
-        public void Deposit(decimal amount)
-        {
-            balance += amount;
-        }
-        public void Withdraw(decimal amount)
-        {
-            if (amount <= balance)
-            {
-                balance -= amount;
-            }
-            else
-            {
-                throw new InvalidOperationException("Insufficient funds");
-            }
-        }
-        public decimal GetBalance()
-        {
-            return balance;
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
