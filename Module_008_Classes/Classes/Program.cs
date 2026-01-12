d8242023dc305953c2f838df82bd1d89210e9c56
NadavAharoni <nadav.aharoni@gmail.com>
2026-01-12 20:43:17 +0200

    Added Module_008_Classes
    Added BankAccount example

---
+++ Module_008_Classes/Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Classes
+{
+    class BankAccount
+    {
+        private decimal balance;
+        public BankAccount(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient funds");
+            }
+        }
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
