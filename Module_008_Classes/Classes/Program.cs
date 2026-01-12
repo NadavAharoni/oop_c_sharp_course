c048834d83084883c9f1b6240cbed58694f2543f
NadavAharoni <nadav.aharoni@gmail.com>
2026-01-12 22:10:51 +0200

    Corrected indentation

--- Module_008_Classes/Classes/Program.cs
+++ Module_008_Classes/Classes/Program.cs
@@ -31,10 +31,10 @@
     {
         static void Main(string[] args)
         {
-        BankAccount account = new BankAccount(1000);
-        account.Deposit(500);
-        account.Withdraw(200);
-        Console.WriteLine($"Current Balance: {account.GetBalance()}");
+            BankAccount account = new BankAccount(1000);
+            account.Deposit(500);
+            account.Withdraw(200);
+            Console.WriteLine($"Current Balance: {account.GetBalance()}");
         }
     }
 }
