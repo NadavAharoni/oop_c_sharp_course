19cf92baf07e995d151b217b5ef869dd64fbe692
NadavAharoni <nadav.aharoni@gmail.com>
2026-01-12 22:44:40 +0200

    Added Expression classes

---
+++ Module_008_Classes/Classes/Expression.cs
@@ -0,0 +1,49 @@
+namespace Classes
+{
+    enum Operand
+    {
+        ADD,
+        SUB,
+        MUL,
+        DIV
+    }
+    abstract class BaseExpression
+    {
+        public abstract double Evaluate();
+    }
+
+    class CompositeExpression : BaseExpression
+    {
+        public BaseExpression e1, e2;
+        public Operand op;
+
+        public override double Evaluate()
+        {
+            switch (op)
+            {
+                case Operand.ADD:
+                    return e1.Evaluate() + e2.Evaluate();
+                case Operand.SUB:
+                    return e1.Evaluate() - e2.Evaluate();
+                case Operand.MUL:
+                    return e1.Evaluate() * e2.Evaluate();
+                case Operand.DIV:
+                    return e1.Evaluate() / e2.Evaluate();
+                default:
+                    throw new System.Exception("Unknown operand");
+            }
+        }
+    }
+    class SimpleExpression : BaseExpression
+    {
+        double number;
+        public SimpleExpression(double number)
+        {
+            this.number = number;
+        }
+        public override double Evaluate()
+        {
+            return number;
+        }
+    }
+}
