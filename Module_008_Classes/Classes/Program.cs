4f0eef49d06bf38fc25a93a886f866e96e8121ba
Nadav Aharoni <65665296+NadavAharoni@users.noreply.github.com>
2026-01-13 21:48:27 +0200

    Added a commented out line
    Documents that new Page() will not compile

--- Module_008_Classes/Classes/Program.cs
+++ Module_008_Classes/Classes/Program.cs
@@ -18,6 +18,7 @@ namespace Classes
             Console.WriteLine($"The result of the expression '{expression}' is: {result}");
 
             Console.WriteLine();
+            // Page p = new Page(); -> this line would cause a compilation error because the constructor is private
             Console.WriteLine("Testing a Singleton Class");
             Page p1 = Page.getThePage();
             Page p2 = Page.getThePage();
